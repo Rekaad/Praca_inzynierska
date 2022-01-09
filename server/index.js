4040cc54ba7cde63f915d83e2485106857e6fded
Radek <gryradek@o2.pl>
2022-01-10 00:55:47 +0100

    Forth commit

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 
 app.use(cors({
-    origin: ["http://localhost:3001"],
-    methods: ["GET", "POST","DELETE","UPDATE"],
-    credentials: true,
-  }));
+origin: ["http://localhost:3000"],
+methods: ["GET", "POST","DELETE","UPDATE"],
+credentials: true,
+}));
 app.use(express.json());
 
 app.use(cookieParser());
@@ -32,17 +32,22 @@ app.use(
 
   
 const bcrypt = require("bcrypt");
+const { query } = require('express');
+//const { hash } = require('bcryptjs');
+//const { hash } = require('bcryptjs');
 const saltRounds = 10;
 
 const db = mysql.createConnection({
 host:"localhost",
 user: "root",
-password: "admin",
+password: "",
 database: "projekt",
+multipleStatements: true
 });
 
+//wyswietlenie wszystkich aktualnosci
  app.get('/aktualnosci', (req, res) =>{
-     db.query('select * from aktualności', (err, result, fields) => {
+     db.query('select * from news', (err, result, fields) => {
          if (err){
              res.send("Błąd!");
          }   
@@ -51,27 +56,7 @@ database: "projekt",
      })
  });
 
-
-// app.get("/", (req, res)=>{
-// const sqlInsert = "INSERT INTO aktualności(idAktualności,Treść) VALUES (?,?)"
-//  db.query(sqlInsert,['5', 'tresc jakas nowa'], (err, res)=>{
-//         if(err){
-//              return console.log(err);
-//          }
-//          return console.log(err);
-//      })
-    
-//  })
-
-//  app.post("/rejestracjatest",(req,res)=>{
-//      db.query("Insert into użytkownik(Login,Hash_haslo) values (?,?)",['login3','haslo1'],(err,res)=>{
-//          if(err){
-//              return console.log(err);
-//          }
-//          return console.log(err);
-//      })
-//  })
-
+//wyswietlenie wszystkich orlikow
 app.get("/orliki",(req,res)=>{
     db.query("select * from orlik", (err,result, fields)=>{
         if(err){
@@ -79,80 +64,264 @@ app.get("/orliki",(req,res)=>{
         }
         res.send(result);
     })
-})
+});
 
-app.get("/szukajpoadresie/:Adres",(req,res)=>{
-    const adres = req.params.Adres;
-    db.query("select * from orlik where Adres like ?",['%'+adres+'%'], (err,result, fields)=>{
+//wyświetlanie orlików z przynajmniej jedna rezerwacja bez przypisanych userow
+app.get("/wolneorliki/:orlik_id",(req,res)=>{
+    const id = req.params.orlik_id;
+    db.query("SELECT * FROM orlik o inner join reservation r on o.orlik_id=r.orlik_id where r.user_id is null and o.orlik_id=?",
+    id, (err,result, fields)=>{
         if(err){
-            res.send({err:err})
+            res.send("Błąd!");
+            console.log(err);
+            res.send(result);
         }
         res.send(result);
     })
-})
+});
 
- app.post("/rejestracja", (req,res)=>{
-     const login = req.body.Login;
-     const haslo = req.body.Hash_haslo;
-
-     bcrypt.hash(haslo, saltRounds, (err, hash) => {
-        if (err) {
-          console.log(err);
+//wyswietlanie wszystkich rezerwacji poprzez dane orlik_id
+app.get("/rezerwacjeorlik/:orlik_id",(req,res)=>{
+    const id = req.params.orlik_id;
+    db.query("SELECT * FROM reservation where orlik_id=?;",
+    id, (err,result, fields)=>{
+        if(err){
+            res.send("Błąd!");
+            console.log(err);
         }
-    
+        res.send(result);
+    })
+});
 
-     const sqlRejestracja = "Insert into użytkownik(Login, Hash_haslo) values (?,?)"
-     db.query(sqlRejestracja,[login, hash],(err,result)=>{
-         if(err){
-             res.send({message:"Taki użytkownik już istnieje"})
-         }
-         res.send(result);
-     }
-    )
+//wyswietlanie wszystkich rezerwacji z danym user_id
+app.get("/rezerwacjeuser/:user_id",(req,res)=>{
+    const id = req.params.user_id;
+    db.query("SELECT * FROM reservation r inner join orlik o on r.orlik_id=o.orlik_id where r.orlik_id=?;",
+    id, (err,result, fields)=>{
+        if(err){
+            res.send("Błąd!");
+            console.log(err);
+        }
+        res.send(result);
+    })
+});
+
+//rezerwowanie 
+app.put("/rezerwacja/:reservation_id/:user_id", (req,res)=>{
+    const resid = req.params.reservation_id;
+    const id = req.params.user_id;
+
+    db.query("update reservation set user_id=? where user_id is null and reservation_id=?",
+    [id,resid],(err,result)=>{
+        console.log(result)
+        if(err){
+            res.send("Błąd");
+            console.log(err);
+        }
+        res.send(result)
+    })
 })
- })
 
-app.post("/login",(req,res)=>{
-    const login = req.body.Login;
-    const haslo = req.body.Hash_haslo;
+//wyswietlenie wszystkich meczy
+app.get("/mecze",(req,res)=>{
+    db.query("SELECT * FROM game g inner join reservation r on g.reservation_id=r.reservation_id;", 
+    (err,result, fields)=>{
+        if(err){
+            res.send("Błąd!")
+        }
+        res.send(result);
+    })
+});
 
-    db.query(
-        "select * from użytkownik where Login=?",[login,haslo],(err,result)=>{
-            if(err){
-                res.send({err:err});
+//zapisanie sie na gre
+app.put("/zapisanienagre/:user_id/:game_id/:reservation_id", (req,res)=>{
+    const userid = req.params.user_id;
+    const gameid = req.params.game_id;
+    const resid = req.params.reservation_id;
+
+    db.query(`
+    update game set reservation_id=?,players=players+1,date=current_date() 
+    where game_id=?;
+    insert into players
+    values (?,?)
+    `,
+    [resid,gameid,userid,gameid],(err,result)=>{
+        if(err){
+            console.log(err)
+            res.send("Błąd");
+            console.log(err);
+        }
+        res.send(result)
+    })
+})
+
+//wypisanie sie z gry
+app.delete("/wypisaniezgry/:user_id/:game_id/:reservation_id", (req,res)=>{
+    const userid = req.params.user_id;
+    const gameid = req.params.game_id;
+    const resid = req.params.reservation_id;
+
+    db.query(`
+    update game set reservation_id=?,players=players-1,date=current_date() 
+    where game_id=?;
+    delete from players where user_id=? and game_id=?
+    `,
+    [resid,gameid,userid,gameid],(err,result)=>{
+        if(err){
+            console.log(err)
+            res.send("Błąd");
+            console.log(err);
+        }
+        res.send(result)
+    })
+})
+
+//wyswietlenie wszystkich uzytkownikow ze wszystkimi rezerwacjami
+app.get("/uzytkownicy",(req,res)=>{
+    db.query("SELECT * FROM user u inner join reservation r on u.user_id=r.user_id;", 
+    (err,result, fields)=>{
+        if(err){
+            res.send("Błąd!")
+        }
+        res.send(result);
+    })
+});
+
+//wyswietlenie konkretnego uzytkownika ze wszystkimi jego rezerwacjami
+app.get("/uzytkownicy/:user_id",(req,res)=>{
+    const id = req.params.user_id;
+    db.query("SELECT * FROM user u inner join reservation r on u.user_id=r.user_id where r.user_id=?;",
+    id, (err,result, fields)=>{
+        if(err){
+            res.send("Błąd!");
+            console.log(err);
+        }
+        res.send(result);
+    })
+});
+
+//rejestracja użytkownika
+
+app.post("/rejestracja", (req,res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+    const surname = req.body.surname;
+    const phone = req.body.phone;
+    console.log(req.body)
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        console.log(hash);
+       if (err) {
+         console.log(err);
+       }
+    const sqlRejestracja = "Insert into user(email,password,name,surname,phone) values (?,?,?,?,?)"
+    db.query(sqlRejestracja,[email, hash,name,surname,phone],(err,result)=>{
+        if(err){
+            console.log(err);
+            res.send({message:"Użytkownik błąd"});
+        }
+
+        res.send(result);
+    }
+   )
+})
+})
+
+app.post('/login', (req,res)=>{
+   
+    const email =req.body.email;
+    const password =req.body.password;
+    
+     db.query(
+         "SELECT * FROM user WHERE email =?",
+         [email],
+         (err,result)=> {
+             if(err)
+            {
+               res.send({ err: err});
+             } 
+             
+
+             if(result.length >0){
+                bcrypt.compare(password, result[0].password, (error, response) => {
+
+                if (response) {
+                    req.session.user= result;
+                    console.log(req.session.user);
+                    res.send(result);
+                  } else {
+                    res.send({ message: "Zły login lub hasło" });
+                  }
+                });
+              } else {
+                
+                res.send({ message: "Użytkownik nie istnieje" });
+              }
             }
+          );
+        });
 
-            if(result.length > 0){
-                bcrypt.compare(haslo, result[0].haslo,(error,response)=>{
-                    if(response){
-                        res.send(result)
-                    }
-                    else{
-                        res.send({message: "Zły login lub hasło"})
-                    }
-                })
+
+
+            
+            
+app.post('/zmianahasla', (req,res)=>{
+   
+    const email = req.body.email;
+    const password = req.body.password;
+
+    console.log(req.body)
+     bcrypt.hash(password, saltRounds, (err, hash) => {
+        console.log(hash);
+      if (err) {
+        console.log(err);
+      }
+     db.query(
+      "UPDATE user SET password =? WHERE email =?",[hash,email],
+         (err,result)=> {
+          if(err)
+          {res.send(result);
+              
+          }else{
+            res.send({ message: "Hasło zostało zapisane!" });
+            console.log(result);
+          }
+   }
+   );
+  });
+});        
+
+//zmiana numeru telefonu
+app.put("/zmiananumeru",(req,res)=>{
+    const email = req.body.email;
+    const phone = req.body.phone;
+    db.query(
+        "update user set phone=? where email=?",[phone,email],(err,result)=>{
+            if(err){
+                res.send(result);
             }
             else {
-                res.send({message: "Dany użytkownik nie istnieje"})
+                res.send({message:"Numer telefonu został zmieniony!"})
             }
         }
     )
-
 })
+    
+
 
 app.get("/zalogowanie",(req,res)=>{
     if(req.session.user){
-        res.send({loggedIn: true, user:req.session.user})
+        res.send({loggedIn: true, user: req.session.user})
     }
     else {
-        res.send({loggedIn:false})
+        res.send({loggedIn: false})
     }
-})
+});
 
 app.get("/wylogowanie",(req,res)=>{
     res.send({loggedIn: false});
     req.session.destroy();
-})
+});
 
 app.put("/uzupelnienieprofilu/:Login",(req,res)=>{
     const login = req.params.Login;
@@ -172,7 +341,27 @@ app.put("/uzupelnienieprofilu/:Login",(req,res)=>{
         }
         res.send(result);
     })
-})
+});
+
+app.get("/informacjeorlik/:idOrlik",(req,res)=>{
+    const id = req.params.idOrlik;
+    db.query("select * from orlik where idOrlik like ?",[id], (err,result, fields)=>{
+        if(err){
+            res.send({err:err})
+        }
+        res.send(result);
+    })
+});
+//do przemyślenia jeszcze jak to rozwiązać
+app.get("/rezerwacja",(req,res)=>{
+    const id = req.params.idOrlik;
+    db.query("select * from orlik where idOrlik like ?",[id], (err,result, fields)=>{
+        if(err){
+            res.send({err:err})
+        }
+        res.send(result);
+    })
+});
 
 app.listen(3001,() => {
     console.log("jestem na porcie 3001");
